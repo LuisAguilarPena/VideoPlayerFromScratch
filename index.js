870b91455fdf52ea645a9c7df6787b9bb0671dbe
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2023-01-09 17:24:31 -0600

    Started implementing time slider

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // Initial declartions 
 const video = document.getElementById('video');
 const videoSrc = 'https://cdn.jwplayer.com/manifests/8Hw3FD28.m3u8';
-const playButton =  document.getElementById('play');
+const playButton = document.getElementById('play');
+const timeSlider = document.getElementById('time-slider');
+
+let duration;
 
 // Check first for native HLS support in Browser else init HLS.js
 if (video.canPlayType('application/vnd.apple.mpegurl')) {
     video.src = videoSrc;
 } else if (Hls.isSupported()) {
-    var hls = new Hls();
+    const hls = new Hls();
     hls.loadSource(videoSrc);
     hls.attachMedia(video);
 }
@@ -18,8 +21,8 @@ const iconSwitching = (iconToShow, iconToHide) => {
     iconToHide.style.display = "none";
 }
 
-// Control Interactions
-playButton.onclick = e => {
+// Button Handlers
+const playVideo = e => {
     if (video.paused) {
         video.play();
         iconSwitching(e.currentTarget.querySelectorAll("i")[1], e.currentTarget.querySelectorAll("i")[0]);
@@ -28,3 +31,11 @@ playButton.onclick = e => {
         iconSwitching(e.currentTarget.querySelectorAll("i")[0], e.currentTarget.querySelectorAll("i")[1]);
     }
 };
+
+// Sliders interaction
+const setTime = () => console.error(', ,  ->', );
+
+// Control Interactions
+playButton.onclick = e => playVideo(e);
+
+timeSlider.oninput = e => setTime(e);
