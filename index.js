d057b88323f4ec7cd72e9a25b041340924d0e9dd
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2023-01-30 16:41:07 -0600

    added volume slider & functionality

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const rewindButton = document.getElementById('rewind');
 const playButton = document.getElementById('play');
 const forwardButton = document.getElementById('forward');
 const muteButton = document.getElementById('mute');
+const volumeSlider = document.getElementById('volume-slider');
 
 let duration;
 
@@ -26,17 +27,8 @@ const iconSwitching = (iconToShow, iconToHide) => {
 
 // Handlers
 const setTime = (val) => video.currentTime = val;
-const rewindVideo = () => video.currentTime -= 5;
-const forwardVideo = () => video.currentTime += 5;
-const muteVideo = e => {
-    video.muted = !video.muted
 
-    if(video.muted) {
-        iconSwitching(e.currentTarget.querySelectorAll("i")[1], e.currentTarget.querySelectorAll("i")[0]);
-    } else {
-        iconSwitching(e.currentTarget.querySelectorAll("i")[0], e.currentTarget.querySelectorAll("i")[1]);
-    }
-}
+const rewindVideo = () => video.currentTime -= 5;
 
 const playVideo = e => {
     if (video.paused) {
@@ -47,6 +39,20 @@ const playVideo = e => {
         iconSwitching(e.currentTarget.querySelectorAll("i")[0], e.currentTarget.querySelectorAll("i")[1]);
     }
 };
+
+const forwardVideo = () => video.currentTime += 5;
+
+const muteVideo = e => {
+    video.muted = !video.muted
+
+    if(video.muted) {
+        iconSwitching(e.currentTarget.querySelectorAll("i")[1], e.currentTarget.querySelectorAll("i")[0]);
+    } else {
+        iconSwitching(e.currentTarget.querySelectorAll("i")[0], e.currentTarget.querySelectorAll("i")[1]);
+    }
+}
+
+const setVolume = (val) => video.volume = val/100;
 
 // Video Events
 video.onloadeddata = e => { //? Set correct time slider values when video has been loaded
@@ -65,3 +71,6 @@ rewindButton.onclick = e => rewindVideo(e);
 playButton.onclick = e => playVideo(e);
 forwardButton.onclick = e => forwardVideo(e);
 muteButton.onclick = e => muteVideo(e);
+volumeSlider.oninput = e => setVolume(e.currentTarget.value);
+
+// video.volume = 1 -> 100%
